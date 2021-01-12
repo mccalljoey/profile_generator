0b8b3c274676bf35b8d5b453dd73f175ebbb4b9c
mccalljoey <mccalljoey@gmail.com>
2021-01-12 22:59:11 +0000

    the messages print out

---
+++ survey.js
@@ -0,0 +1,42 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+
+rl.question('What\'s your name?', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+  rl.close();
+rl.question('What\'s an activity you like doing? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+    
+  rl.close();
+rl.question('What do you listen to while doing that? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+      
+  rl.close();
+rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+        
+  rl.close();
+rl.question('What\'s your favourite thing to eat for that meal? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+          
+  rl.close();
+rl.question('Which sport is your absolute favourite? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+            
+  rl.close();
+rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+            
+  rl.close();
+});
